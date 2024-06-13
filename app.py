78ad15d0cde683be9c0820e81ac22d5824b6b287
Anjalishukla-18 <anjalishukla2332@gmail.com>
2024-06-13 14:42:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,67 +1,74 @@
 import streamlit as st
 import pickle
 import numpy as np
-import pandas
+import pandas as pd
 
-Ridge=pickle.load(open("Ridge.pkl","rb"))
-Lasso=pickle.load(open("Lasso.pkl","rb"))
-KNN=pickle.load(open("KNN.pkl","rb"))
+# Load models
+Ridge = pickle.load(open("Ridge.pkl", "rb"))
+Lasso = pickle.load(open("Lasso.pkl", "rb"))
+KNN = pickle.load(open("KNN.pkl", "rb"))
 
-st.title('car_details')
- 
-st.subheader('information about car')
- 
-options=st.sidebar.selectbox('Select ML Model',['Ridge','KNeighborsRegressor','Lasso'])
- 
-# name=st.slider()
-year=st.slider(1992,2020) 
-selling_price=st.slider(2.000000e+04,8.900000e+06)
-km_driven=st.slider(1.000000,806599.000000)
-fuel=st.selectbox('fule',['Petrol','Diesel','CNG'])
-seller_type=st.selectbox('seller_type',['Individual','Dealer','Trustmark Dealer'])
-transmission=st.selectbox('transmission',['Manual','Automatic']) 
-owner=st.selectbox('owner',['First Owner','Second Owner','Third Owner','Fourth & Above Owner','Test Drive Car']) 
- 
- 
-if st.button('predict'): 
-    
+st.title('Car Details')
+
+st.subheader('Information about Car')
+
+# Sidebar options for model selection
+options = st.sidebar.selectbox('Select ML Model', ['Ridge', 'KNeighborsRegressor', 'Lasso'])
+
+# Input sliders and selectors
+year = st.slider('Year', 1992, 2020, step=1)
+selling_price = st.slider('Selling Price', 20000, 8900000, step=1000)
+km_driven = st.slider('Kilometers Driven', 1000, 806599, step=100)
+fuel = st.selectbox('Fuel', ['Petrol', 'Diesel', 'CNG'])
+seller_type = st.selectbox('Seller Type', ['Individual', 'Dealer', 'Trustmark Dealer'])
+transmission = st.selectbox('Transmission', ['Manual', 'Automatic'])
+owner = st.selectbox('Owner', ['First Owner', 'Second Owner', 'Third Owner', 'Fourth & Above Owner', 'Test Drive Car'])
+
+# Prediction button
+if st.button('Predict'):
+    # Encoding categorical variables
     if fuel == "Petrol":
-        fuel=0
-    elif fuel =="Diesel":
-        fuel=1
+        fuel = 0
+    elif fuel == "Diesel":
+        fuel = 1
     else:
-        fuel=2
-    if seller_type =="Individual":
-        seller_type=0
-    elif seller_type=="Dealer":
-        seller_type=1
+        fuel = 2
+
+    if seller_type == "Individual":
+        seller_type = 0
+    elif seller_type == "Dealer":
+        seller_type = 1
     else:
-        seller_type=2
-    if transmission =="Manual":
-      transmission=0
+        seller_type = 2
+
+    if transmission == "Manual":
+        transmission = 0
     else:
-      transmission=1
-    if owner =="First Owner":
-        Owner=0
-    elif owner =="Second Owner":
-        Owner=1
-    elif owner =="Third Owner":
-        Owner=2
-    elif owner=="Fourth & Above Owner":
-        Owner=3
+        transmission = 1
+
+    if owner == "First Owner":
+        owner = 0
+    elif owner == "Second Owner":
+        owner = 1
+    elif owner == "Third Owner":
+        owner = 2
+    elif owner == "Fourth & Above Owner":
+        owner = 3
     else:
-        Owner=4
-        
-        
-    test = np.array([year,selling_price,km_driven,fuel,seller_type,transmission,owner])
-    test = test.reshape(1,7)
+        owner = 4
+
+    # Create the test array
+    test = np.array([year, selling_price, km_driven, fuel, seller_type, transmission, owner])
+    test = test.reshape(1, -1)
+
+    # Model prediction
     if options == "Ridge":
-        st.success(Ridge.predict(test)[0])
-    elif options =="KNeighborsRegressor":
-        st.success(KNN.predict(test)[0])
+        prediction = Ridge.predict(test)[0]
+    elif options == "KNeighborsRegressor":
+        prediction = KNN.predict(test)[0]
     else:
-        st.success(Lasso.predict(test)[0])
-                
-     
- 
- 
+        prediction = Lasso.predict(test)[0]
+
+    # Display the result
+    st.success(f'Predicted Price: {prediction}')
+
